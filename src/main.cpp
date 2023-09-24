96456bb8dd139dcab2797cd69cbcb4d419c6abac
foxidokun <fox@forestfox.tk>
2023-09-24 16:12:55 +0300

    make animation smooth

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,18 @@
 #include "common.h"
 #include <SFML/Graphics.hpp>
 #include "render.h"
+#include <chrono>
+#include <thread>
+
+namespace chrono = std::chrono;
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), WINDOW_TITLE);
 
 Gas gas(Interval(0, 100), Interval(0, 100));
     gas.add(new NyaMolec(Point(30, 30, 0), Vector(0.1, 0.1, 0), 20));
+
+    auto frame_start_time = chrono::system_clock::now();
 
     while (window.isOpen()) {
         sf::Event event;
@@ -23,5 +29,7 @@ Gas gas(Interval(0, 100), Interval(0, 100));
 
         // display
         window.display();
+
+        std::this_thread::sleep_until(frame_start_time + std::chrono::milliseconds(33));
     }
 }
